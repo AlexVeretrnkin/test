ce50d8b30087c13df1705fb2df44680987b6692d
alexveretenkin <alexveretenkin@cybercube.gg>
2019-12-04 19:35:06 +0200

    Init

--- src/main/java/ua/kpi/tef/ti71/observer/SaveListener.java
+++ src/main/java/ua/kpi/tef/ti71/observer/SaveListener.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SaveListener implements EventListener {
-
     public SaveListener() {
         // It's empty
     }
